4917055da641a0dca5427fb221dfd9386835988c
james_folk <jamesfolk1@gmail.com>
2014-10-16 14:54:18 -0400

    fixed the build script for emscripten.

--- emscripten/build.sh
+++ emscripten/build.sh
@@ -2,10 +2,18 @@
 
 echo "building emscripten..."
 
+#emmake make -j 8
+
+
 MY_APP_NAME=JLIGameEngineTest
-MY_CORE_PATH=../../../third_party/platform/core
-MY_PLATFORM_PATH=../../../third_party/platform/emscripten
+MY_THIRD_PARTY_RELATIVE_PATH=../../../third_party
+MY_ASSET_DIR=../COMMON/assets
+MY_EMSCRIPTEN_PATH=$MY_THIRD_PARTY_RELATIVE_PATH/platform/emscripten
+MY_CORE_PATH=$MY_THIRD_PARTY_RELATIVE_PATH/platform/core
 
-emcc -Wall -I. -I$MY_CORE_PATH -I$MY_PLATFORM_PATH main.cpp $MY_PLATFORM_PATH/Log.cpp $MY_CORE_PATH/Game.cpp -o $MY_APP_NAME.html
+CFLAGS="-I. -I$MY_EMSCRIPTEN_PATH/ -I$MY_CORE_PATH/ -Wall -Wextra"
+
+SOURCES="main.cpp $MY_CORE_PATH/Game.cpp $MY_EMSCRIPTEN_PATH/File.cpp $MY_EMSCRIPTEN_PATH/Input.cpp $MY_EMSCRIPTEN_PATH/Log.cpp $MY_EMSCRIPTEN_PATH/Sound.cpp"
+
+emcc $CFLAGS $SOURCES -o $MY_APP_NAME.html
 open $MY_APP_NAME.html
-
--- emscripten/main.cpp
+++ emscripten/main.cpp
@@ -1,72 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <GL/glfw.h>
-#include <time.h>
 #include <emscripten/emscripten.h>
-#include "Game.h"
-#include "Log.h"
+#include "../../../third_party/platform/core/Game.h"
 
-int init_gl(void);
+int init_gl();
 void do_frame();
 void shutdown_gl();
 
-const int width = 480;
-const int height = 800;
-double last_time = 0;
+const int width = 480,
+height = 800;
 
-double get_time()
+int main()
 {
-	clock_t uptime = clock() / (CLOCKS_PER_SEC / 1000);
-	return uptime;
-}
-
-int main(void) 
-{
-	last_time = get_time();
-
-	if (init_gl() == GL_TRUE) 
-	{
-        create();
+    if (init_gl() == GL_TRUE) {
         
-		resize(0, 0, width, height);
-        
-
-		emscripten_set_main_loop(do_frame, 0, 1);
-	}
-		
-	shutdown_gl();
-
-	return 0;
+        resize(0, 0, width, height);
+        emscripten_set_main_loop(do_frame, 0, 1);
+    }
+    
+    shutdown_gl();
+    
+    return 0;
 }
 
 int init_gl()
 {
-	if (glfwInit() != GL_TRUE) {
-		printf("glfwInit() failed\n");
-		return GL_FALSE;
-	}
-
-	if (glfwOpenWindow(width, height, 8, 8, 8, 8, 16, 0, GLFW_WINDOW) != GL_TRUE) 
-	{
-		printf("glfwOpenWindow() failed\n");
-    		return GL_FALSE;
-	}
-
+    
+    
+    if (glfwInit() != GL_TRUE) {
+        printf("glfwInit() failed\n");
+        return GL_FALSE;
+    }
+    
+    if (glfwOpenWindow(width, height, 8, 8, 8, 8, 16, 0, GLFW_WINDOW) != GL_TRUE) {
+        printf("glfwOpenWindow() failed\n");
+        return GL_FALSE;
+    }
+    
+    create();
+    
     return GL_TRUE;
 }
 
 void do_frame()
-{	
-	double current_time = get_time();
-	update((current_time - last_time) / 1000.0f);
-	last_time = current_time;
-
-	render();
-	glfwSwapBuffers();
+{
+    update(0.16);render();
+    glfwSwapBuffers();
 }
 
 void shutdown_gl()
 {
-	destroy();
-	glfwTerminate();
+    destroy();
+    glfwTerminate();
 }
