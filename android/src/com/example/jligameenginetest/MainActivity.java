4da5ebcdddf2fa642fd5e72a85bbe0d0ae839718
james_folk <jamesfolk1@gmail.com>
2014-10-17 01:31:48 -0400

    fixed the file support for android

--- android/src/com/example/jligameenginetest/MainActivity.java
+++ android/src/com/example/jligameenginetest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 
     @Override protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-//        JLIGameEngineTestLib.initAssetManager(getAssets());
+        JLIGameEngineTestLib.initAssetManager(getAssets());
         mView = new GLView(getApplication());
 	setContentView(mView);
     }
