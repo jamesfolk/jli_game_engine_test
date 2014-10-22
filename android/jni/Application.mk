5662169b834adb42ab478e694192ce8221246bd0
james_folk <jamesfolk1@gmail.com>
2014-10-22 11:45:59 -0400

    made sure the project compiles with out the static libraries

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_MODULES      := game \
+APP_MODULES      := game #\
                     bullet \
                     jli \
                     jpg \
--- android/jni/game/Android.mk
+++ android/jni/game/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 MY_THIRD_PARTY_RELATIVE_PATH := /Users/jamesfolk/Dropbox/GameDevelopment/mygames/third_party
 #MY_THIRD_PARTY_RELATIVE_PATH := $(LOCAL_PATH)/../../../../../third_party/
 
-LOCAL_STATIC_LIBRARIES := bullet \
+LOCAL_STATIC_LIBRARIES := #bullet \
                           jli \
                           jpg \
                           lua \
@@ -21,8 +21,21 @@ LOCAL_SRC_FILES := jni.cpp \
                    $(MY_THIRD_PARTY_RELATIVE_PATH)/platform/android/Sound.cpp
 
 LOCAL_C_INCLUDES := $(MY_THIRD_PARTY_RELATIVE_PATH)/platform/core/ \
-                    $(MY_THIRD_PARTY_RELATIVE_PATH)/platform/android/ \
-                    $(MY_THIRD_PARTY_RELATIVE_PATH)/bullet/ \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/platform/android/ #\
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/bullet/bullet-2.82-r2704/src/ \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletCollision/BroadphaseCollision \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletCollision/CollisionDispatch \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletCollision/CollisionShapes \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletCollision/Gimpact \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletCollision/NarrowPhaseCollision \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletDynamics/Character \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletDynamics/ConstraintSolver \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletDynamics/Dynamics \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletDynamics/Featherstone \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletDynamics/MLCPSolvers \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletDynamics/Vehicle \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/BulletSoftBody \
+                    $(MY_THIRD_PARTY_RELATIVE_PATH)/LinearMath #\
                     $(MY_THIRD_PARTY_RELATIVE_PATH)/jli_game_engine/ \
                     $(MY_THIRD_PARTY_RELATIVE_PATH)/jpg/ \
                     $(MY_THIRD_PARTY_RELATIVE_PATH)/lua/ \
